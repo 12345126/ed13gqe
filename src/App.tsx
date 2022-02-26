ef16516c1342c07013e7b16183c9191bd9da1656
123456123 <ymsmmsyms@gmail.com>
2022-02-26 12:45:56 +0200

    Initilization with goods dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,19 @@ export const App = () => {
 
   // dummy data
   //--------------------------------------------------
-  // const good1 = new GoodClass()
-  // good1.currentName = 'good_1'
-  // good1.currentPrice = 12.49
-  // good1.currentIsImported = true
-  // good1.currentType = GoodClass.validTypes[0]
-  // goodsInitialArray.push(good1)
+  const good1 = new GoodClass()
+  good1.currentName = 'good_1'
+  good1.currentPrice = 12.49
+  good1.currentIsImported = true
+  good1.currentType = GoodClass.validTypes[0]
+  goodsInitialArray.push(good1)
 
-  // const good2 = new GoodClass()
-  // good2.currentName = 'good_2'
-  // good2.currentPrice = 5.99
-  // good2.currentQuantity = 3
-  // good2.currentType = GoodClass.validTypes[GoodClass.validTypes.length-1]
-  // goodsInitialArray.push(good2)
+  const good2 = new GoodClass()
+  good2.currentName = 'good_2'
+  good2.currentPrice = 5.99
+  good2.currentQuantity = 3
+  good2.currentType = GoodClass.validTypes[GoodClass.validTypes.length-1]
+  goodsInitialArray.push(good2)
   //--------------------------------------------------
 
   const { array: goods, set:setGood, push:addGood, clear:removeAllGoods } = useArray(goodsInitialArray)
